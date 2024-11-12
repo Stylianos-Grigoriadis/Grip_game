8b9b61c41d6bacdac738f61404b2ccbed0ea7b52
Stylianos <stylgrig@yahoo.gr>
2024-11-12 16:41:04 +0200

    Analysis of first participant

--- User_naming_files.py
+++ User_naming_files.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import glob
+import numpy as np
 
 
 # Define the directory path
@@ -27,9 +28,12 @@ directory_path = r'C:\Users\Stylianos\OneDrive - Αριστοτέλειο Παν
 # Isometric_80_T1
 # Isometric_80_T2
 # Isometric_80_T3
-# Pert_down
-# Pert_up
-max = 31.9
+# Pert_down_T1
+# Pert_down_T2
+# Pert_up_T1
+# Pert_up_T2
+
+max = 43.2
 print(f'5% : {0.05*max}')
 print(f'20% : {0.2*max}')
 print(f'40% : {0.4*max}')
@@ -41,7 +45,9 @@ for file in files:
     file_name = os.path.basename(file)
     print(file_name)
 
+
     signal = pd.read_csv(file, skiprows=2)
+    print(np.mean(signal['Performance'][200:300]))
     plt.plot(signal['Performance'])
     plt.title(file_name)
     plt.show()
