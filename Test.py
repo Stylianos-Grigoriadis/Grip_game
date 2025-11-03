5341c7ca5bacbc50b632a274ea1d92b75d1e3dd0
Stylianos-Grigoriadis <stylgrig@yahoo.gr>
2025-11-03 21:30:27 +0200

    Graphs creation

--- Test.py
+++ Test.py
@@ -1,11 +1,50 @@
 import Lib_grip as lb
 import lib
 import colorednoise as cn
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-data = cn.powerlaw_psd_gaussian(1,1000)
+N=200
+time = np.linspace(0,10,N)
+
+brown = cn.powerlaw_psd_gaussian(3,N)
+random = np.random.rand(N)
+brown_random = brown
+brown_z = lb.z_transform(brown_random,5,40)
+pink = lb.fgn_sim(N,0.99)
+pink_z = lb.z_transform(pink,2.5,40)
+
+SaEn_brown = round(lb.Ent_Samp(brown_z, 2, 0.2),2)
+SaEn_pink = round(lb.Ent_Samp(pink_z, 2, 0.2),2)
+SD_brown = round(np.std(brown_z),2)
+SD_pink = round(np.std(pink_z),2)
+
+# plt.plot(brown_z, label=f'Signal 1, SaEn = {SaEn_brown}, SD = {SD_brown}')
+# plt.plot(pink_z, label=f'Signal 2, SaEn = {SaEn_pink}, SD = {SD_pink}')
+# plt.legend()
+# plt.show()
 
 
-lib.DFA_nolds(data, 16, 9)
-scales, fluctuation, alpha = lib.DFA_NONAN(data, range(16,int(len(data)/9)))
-print(alpha)
+plt.figure(figsize=(8, 4))
+plt.plot(time, brown_z, color='red', linewidth=3, label=f'Signal 1, SaEn = {SaEn_brown}, SD = {SD_brown}')
+plt.plot(time, pink_z, color='blue', linewidth=3, label=f'Signal 2, SaEn = {SaEn_pink}, SD = {SD_pink}')
+
+# Styling
+plt.legend(
+    fontsize=30,
+    loc='upper right',
+    frameon=True,
+    fancybox=True,
+    shadow=True,
+    framealpha=0.9,
+    prop={'weight': 'bold'}  # <-- makes legend text bold
+)
+
+plt.title('Signals Comparison', fontsize=14, fontweight='bold')
+plt.xlabel('Time (s)', fontsize=12, fontweight='bold')
+plt.ylabel('Force output (N)', fontsize=12, fontweight='bold')
+
+plt.tight_layout()
+plt.show()
+
